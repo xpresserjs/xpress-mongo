3fccab960a3489d6b9a9c71660eb28be10c4e273
trapcodeio <trapcode.io@gmail.com>
2020-10-29 08:19:00 +0100

    Updated Packages

--- tests/test.js
+++ tests/test.js
@@ -4,28 +4,28 @@ async function run() {
     await connector();
     const {Users, Contacts} = require('./models');
 
-    // 5f8bb34c17793a7bb278d24f
-    const user = await Users.findById('5e5acba088ebeef8a715ca43', {
-        // projection: {_id: 0}
-    });
-
-
-    // user.set('contact.date', new Date());
-
-    console.log(await user.save());
+    // // 5f8bb34c17793a7bb278d24f
+    // const user = await Users.findById('5e5acba088ebeef8a715ca43', {
+    //     // projection: {_id: 0}
+    // });
+    //
+    //
+    // // user.set('contact.date', new Date());
+    //
+    // console.log(await user.save());
 
     /**
      * Async Space
      */
-    // const guest = new Users().set({
-    //     type: 'guest',
-    //     first_name: 'Hello',
-    //     last_name: 'World',
-    //     guestId: '678',
-    //     updated_at: 'Fri, 03 Apr 2020 00:00:00 GMT'
-    // });
-    //
-    // console.log(guest);
+    const guest = new Users().set({
+        type: 'guest',
+        first_name: 'Hello',
+        last_name: 'World',
+        guestId: '678',
+        updated_at: 'Fri, 03 Apr 2020 00:00:00 GMT'
+    });
+
+    console.log(guest);
 
     /**
      * End Async Space
