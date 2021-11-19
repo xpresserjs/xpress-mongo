9ab2919bd1fa42d56a76e8ed8c68e925000636ed
trapcodeio <trapcode.io@gmail.com>
2021-11-19 19:17:26 +0100

    Refactored and removed dist folder

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,9 @@
 import Connector from "./connection";
 import User from "./models/User";
 import Songs from "./models/Songs";
-import XMongoTypedModel = require("../src/XMongoTypedModel");
-import { omitIdAndPick } from "../index";
-import Chance from "chance";
+// import Chance from "chance";
 
-const chance = new Chance();
+// const chance = new Chance();
 
 async function Main() {
     const connection = await Connector();
@@ -25,21 +23,11 @@ async function Main() {
         username: "John",
         email: "hello@good.com",
         firstName: "John",
-        lastName: "Smith",
-        contact: {
-            a: {
-                b: {
-                    c: "foo"
-                },
-                d: "dog"
-            }
-        }
+        lastName: "Smith"
     });
 
-    console.log("1", song.data.contact.a.b.c);
-    console.log("2", song.get("contact.a.b.c"));
     //
-    // // console.log(song);
+    console.log(song);
     // const song: User | null = await User.native().findOne({});
     //
     // if (!song) throw Error("No Song found");
