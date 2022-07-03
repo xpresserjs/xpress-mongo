d736a1c4d2e5be60d3e40a055348fafcd74f8347
trapcodeio <trapcode.io@gmail.com>
2022-07-03 19:49:02 +0100

    add paginated file

---
+++ src/types/pagination.ts
@@ -0,0 +1,20 @@
+export interface PaginatedMeta {
+    total: number;
+    perPage: number;
+    page: number;
+    lastPage: number;
+}
+
+export interface Paginated<T = any> extends PaginatedMeta {
+    data: T[];
+};
+
+export function Paginated<T>(): Paginated<T> {
+    return {
+        total: 0,
+        perPage: 0,
+        page: 1,
+        lastPage: 1,
+        data: [] as T[]
+    };
+}
